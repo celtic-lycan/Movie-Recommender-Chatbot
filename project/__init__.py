71511f9c169124aa544053b2fe0fff07ad749dc5
Pranav Tripathi <tripathipranav14@gmail.com>
2022-12-25 13:32:54 +0530

    probably final commit

--- project/__init__.py
+++ project/__init__.py
@@ -1,29 +1,34 @@
+"""Initialize app."""
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-# initialising SQLAlchemy
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app():
-    app = Flask(__name__)
+    """Construct the core app object."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object("config.Config")
 
-    app.config["SECRET_KEY"] = '99p0EeuJlqYLnq-zZVMBMw'
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-
+    # Initialize Plugins
     db.init_app(app)
-
-    from . import models
+    login_manager.init_app(app)
 
     with app.app_context():
+        from . import auth, routes
+        from .assets import compile_static_assets
+
+        # Register Blueprints
+        app.register_blueprint(routes.main_bp)
+        app.register_blueprint(auth.auth_bp)
+
+        # Create Database Models
         db.create_all()
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+        # Compile static assets
+        if app.config["FLASK_ENV"] == "development":
+            compile_static_assets(app)
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
+        return app
